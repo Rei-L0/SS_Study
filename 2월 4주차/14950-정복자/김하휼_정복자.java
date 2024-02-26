f15d0e8aea3fa98121be36e605933e3e9ddf26d1
Busangangster <83652134+busangangster@users.noreply.github.com>
2024-02-26 22:46:14 +0900

    [Solved] 14950

---
+++ 2월 4주차/14950-정복자/김하휼_정복자.java
@@ -0,0 +1,84 @@
+package Baekjoon.최소신장트리;
+import java.util.*;
+import java.io.*;
+
+// 프림으로 풀었습니다. 
+class P14950 {
+    static int N,M,T,ans;
+    static boolean[] visited;
+    static ArrayList<ArrayList<Edge>> graph;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+        
+        graph = new ArrayList<ArrayList<Edge>>();
+        ans = 0;
+        visited = new boolean[N+1];
+
+        for (int i=0; i<=N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+            graph.get(A).add(new Edge(B, C));
+            graph.get(B).add(new Edge(A, C));
+        }
+
+        prim(1);
+        System.out.println(ans);
+
+     }
+
+     public static void prim(int start) {
+        PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
+        pq.offer(new Edge(start, 0));
+        int idx = 1; // 정복할 때마다 t씩 증가하는데, t*idx만큼 증가시키기 위한 변수 
+        int cnt = 0; // 정복한 횟수 
+
+        while (!pq.isEmpty()) {
+            Edge cur = pq.poll();
+
+            if (visited[cur.end]) continue;
+
+            visited[cur.end] = true;
+
+            if (cnt >= 2){ // 정복 횟수가 2 이상이면, 도로의 비용을 t*idx만큼 증가한 값을 총 비용에 +
+                ans += cur.cost + (T*idx);
+                idx++;
+            }
+            else { // 아니면 기존 도로의 비용만큼만 더해줌 
+                ans += cur.cost;
+            }
+
+            for (Edge next: graph.get(cur.end)) {
+                if (!visited[next.end]) {
+                    pq.offer(new Edge(next.end, next.cost));
+                }    
+            }
+            cnt++;
+        }
+     }
+}
+
+class Edge implements Comparable<Edge>{
+    int end;
+    int cost;
+
+    public Edge(int end, int cost) {
+        this.end = end;
+        this.cost = cost;
+    }
+    @Override
+    public int compareTo(Edge o){
+        return this.cost - o.cost;
+    }
+}
