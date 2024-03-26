d8f2447d0a6438759b8e3a1d697dbfc0365817b5
yunhoo <qkrdusgn00@naver.com>
2024-03-26 10:35:58 +0900

    [UnSolved] 3048

---
+++ 3월 3주차/3048-개미/박연후_개미.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+// 아직 못 풀었습니다.. 금방 풀도록 하겠습니다..
+public class Main { 
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+	}
+}
