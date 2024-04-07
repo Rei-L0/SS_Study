c7fa4be1b9dadfa577f2ba03143a8522b6fcf58e
sanadoing <143790326+sanadoing@users.noreply.github.com>
2024-04-07 18:01:35 +0900

    [Solved] 17299

--- 4월 1주차/17299-오등큰수/이산하_오등큰수.java
+++ 4월 1주차/17299-오등큰수/이산하_오등큰수.java
@@ -6,7 +6,11 @@ import java.util.Deque;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class Main {
+// 시간초과를 해결한 방법
+// 1. 빈도수를 저장하는 HashMap -> 배열
+// 2. stringbuilder 사용
+
+public class B_17299 {
 
 	public static void main(String[] args) throws Exception {
 
@@ -28,6 +32,10 @@ public class Main {
 		int[] result = new int[N];
 
 		StringBuilder sb = new StringBuilder();
+
+		// 반대로 하나씩 넣으면서 지금 들어오는 수와 가장 위에 있는 수의 빈도수를 비교하여
+		// 현재 들어온 수의 빈도수가 가장 위에 있는 수의 빈도수보다 작으면
+		// 현재 들어온 수의 result_idx 값에 가장 위에 있는 수를 넣어줌.
 		for (int i = N - 1; i >= 0; i--) {
 			int now = numbers[i];
 
