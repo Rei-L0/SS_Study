7e7196d4fecef54a00296c162ec9864c3bf143cb
Rei-L0 <qkrdusgn00@naver.com>
2024-04-14 18:21:56 +0900

    [Unsolved] 1946

---
+++ 4월 2주차/1946-신입 사원/박연후-신입사원.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ 빨리 풀도록 하겠습니다.
+ */
+
+public class Main {
+
+	static BufferedReader br;
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+
+
+	public static void main(String[] args) throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+
+		st = new StringTokenizer(br.readLine());
+
+	}
+}
