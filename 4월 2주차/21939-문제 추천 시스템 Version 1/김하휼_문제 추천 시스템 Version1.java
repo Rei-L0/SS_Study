154a1c3aa14e4a62bd287b839fa7b1e44bfab56b
Busangangster <83652134+busangangster@users.noreply.github.com>
2024-04-14 17:12:15 +0900

    [Solved] 21939

--- 4월 2주차/21939-문제 추천 시스템 Version 1/김하휼_문제 추천 시스템 Version1.java
+++ 4월 2주차/21939-문제 추천 시스템 Version 1/김하휼_문제 추천 시스템 Version1.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class BJ_G4_21939_문제추천시스템 {
+public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
