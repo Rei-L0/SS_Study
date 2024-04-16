56ba1d8aec0c87f3a1b4379792aa1d3d4ebd62c7
Rei-L0 <qkrdusgn00@naver.com>
2024-04-16 22:44:14 +0900

    [Solved] 1946

--- 4월 2주차/1946-신입 사원/박연후-신입사원.java
+++ 4월 2주차/1946-신입 사원/박연후-신입사원.java
@@ -1,22 +1,74 @@
 import java.io.*;
 import java.util.*;
 
-/*
- 빨리 풀도록 하겠습니다.
- */
-
 public class Main {
 
-	static BufferedReader br;
-	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
 
+    static int t, n, ans;
 
+    static Score[] num;
 
-	public static void main(String[] args) throws Exception {
-		br = new BufferedReader(new InputStreamReader(System.in));
+    static class Score implements Comparable<Score> {
 
-		st = new StringTokenizer(br.readLine());
+        int x;
+        int y;
 
-	}
+        public Score(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Score o) {
+            if (this.x == o.x) {
+                return Integer.compare(this.y, o.y);
+            }
+            return Integer.compare(this.x, o.x);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
+        t = Integer.parseInt(st.nextToken());
+
+        for (int z = 0; z < t; z++) {
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+
+            num = new Score[n];
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                num[i] = new Score(a, b);
+            }
+
+            Arrays.sort(num);
+
+            int f = 0;
+            int s = 0;
+            ans = 0;
+            for (int i = 0; i < n; i++) {
+                Score now = num[i];
+                if (f == 0 && s == 0) {
+                    ans++;
+                    f = now.x;
+                    s = now.y;
+                } else {
+                    if (f > now.x || s > now.y) {
+                        ans++;
+                        f = now.x;
+                        s = now.y;
+                    }
+                }
+            }
+            sb.append(ans).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
