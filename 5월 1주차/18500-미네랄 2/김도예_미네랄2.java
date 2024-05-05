cfeee7fe4f6b5356f123d46b2a639b4a5276091a
김도예 <kmdy125@gmail.com>
2024-05-05 19:44:40 +0900

    [Solved] 18500

--- 5월 1주차/18500-미네랄 2/김도예_미네랄2.java
+++ 5월 1주차/18500-미네랄 2/김도예_미네랄2.java
@@ -6,9 +6,6 @@ import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-/*
- * 반례나 예제는 다 맞는데 틀려서 고민 중
- */
 public class Main {
 
 	static int R, C, N;
@@ -41,7 +38,7 @@ public class Main {
 			for(int j = 0; j < C; j++) {
 				sb.append(map[i][j]);
 			}
-			if(i != R-1) sb.append("\n");
+			sb.append("\n");
 		}
 		System.out.println(sb);
 	}
@@ -66,53 +63,50 @@ public class Main {
 			}
 		}
 	}
-	// 미네랄 사라진 후 끊긴 게 있는지 확인
+	// 미네랄 사라진 후 바닥에서부터 bfs로 visit check 해서 끊긴 부분 확인
 	static void check(int y, int x) {
-		checkNode = new ArrayList<>();
-		for(int d = 0; d < 4; d++) {
-			int ny = y + dy[d];
-			int nx = x + dx[d];
-			if(ny < 0 || nx < 0 || ny >= R || nx >= C || map[ny][nx] == '.') continue;
-			checkNode.add(new Node(ny, nx));
+		visit = new boolean[R][C];
+		list = new ArrayList<>();
+		for(int i = 0; i < C; i++) {
+			if(map[R-1][i] == 'x') air(R-1, i);
 		}
 		
-		for(Node n : checkNode) {
-			if(air(n.y, n.x)){
-				for(Node node : list) {
-					map[node.y][node.x] = '.';
+		for(int i = 0; i < R; i++) {
+			for(int j = 0; j < C; j++) {
+				if(!visit[i][j] && map[i][j] == 'x') {
+					list.add(new Node(i, j));
 				}
-				down();
 			}
 		}
+		if(!list.isEmpty()) {
+			for(Node n : list) {
+				map[n.y][n.x] = '.';
+			}
+			down();
+		}
 	}
-	// 바닥에 닿으면 false 반환
-	static boolean air(int y, int x){
-		visit = new boolean[R][C];
+	// visit check
+	static void air(int y, int x){
 		Queue<Node> q = new ArrayDeque<>();
-		list = new ArrayList<>();
 		q.offer(new Node(y, x));
-		list.add(new Node(y, x));
 		visit[y][x] = true;
 		while(!q.isEmpty()) {
 			Node node = q.poll();
-			if(node.y == R-1) return false;
 			for(int d = 0; d < 4; d++) {
 				int ny = node.y + dy[d];
 				int nx = node.x + dx[d];
 				if(ny < 0 || nx < 0 || ny >= R || nx >= C || visit[ny][nx] || map[ny][nx] == '.') continue;
 				visit[ny][nx] = true;
 				q.offer(new Node(ny, nx));
-				list.add(new Node(ny, nx));
 			}
 		}
-		return true;
 	}
 	static void down() {
 		boolean flag = false;
 		while(true) {
 			for(Node node : list) {
-				node.y++;
-				if(node.y + 1 >= R || map[node.y + 1][node.x] == 'x') {
+				node.y = node.y + 1;
+				if(node.y + 1 == R || map[node.y + 1][node.x] == 'x') {
 					flag = true;
 				}
 			}
