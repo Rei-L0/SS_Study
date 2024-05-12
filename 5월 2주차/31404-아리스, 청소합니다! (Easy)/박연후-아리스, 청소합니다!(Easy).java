34697a985c028b4a01ec21f6c9bd02acee5521a5
Rei <qkrdusgn00@naver.com>
2024-05-12 18:58:55 +0900

    [UnSolved] 31404

---
+++ 5월 2주차/31404-아리스, 청소합니다! (Easy)/박연후-아리스, 청소합니다!(Easy).java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+// 정처기 준비하느라 못 풀었습니다.. 죄송합니다 
+
+public class Main {
+
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+	}
+
+}
